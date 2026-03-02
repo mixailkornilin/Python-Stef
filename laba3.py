704c7895187075ab0192dbd10fba8929b70e068a
mixailkornilin <mixail.kornilin@gmail.com>
2026-03-02 16:04:19 +0300

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -3,20 +3,21 @@ predmet = ['Русский','Математика','География']
 date = ['18.10.2013','01.01.2013','12.02.2014']
 dati = ['2','3','4','5']
 
-second_name_hash = hash(second_name[0])
-predmet_hash = hash(predmet[0])
-date_hash = hash(date[0])
-dati_hash = hash(dati[0])
-print(second_name_hash)
+masiv_hash = dict()
+
+
 for s in date:
     if s == '18.10.2013':
         for i in predmet:
             if i == 'Русский':
                 for g in second_name:
                      for h in dati:
-                        print(s, i,g,h)
+                        print(s, i,g)
+                        masiv_hash[hash(s + i + g)] = h
 
 
+print(hash('18.10.2013' + 'Русский'+'Корнилин'))
+print(masiv_hash[hash('18.10.2013'+'Русский' +'Корнилин' )])
 #Фильтр через хеширование
 #фамилия > предмет(повторяетс) > дата(повторяетс) > пустое значение(оценка) не повторяется
 #фамилия > предмет > дата > пустое значение
