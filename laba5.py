dac5ecbc71b9946cbd613bf5377fe0355fd2d3ce
mixailkornilin <mixail.kornilin@gmail.com>
2026-04-06 15:05:51 +0300

    Update laba5.py

--- laba5.py
+++ laba5.py
@@ -1,23 +1,30 @@
-second_name = ['Корнилин', 'Клейменов', 'Млюкова']
-predmet = ['Русский','Математика','География']
-date = ['18.10.2013','01.01.2013','12.02.2014']
-dati = ['2','3','4','5']
+def R_S(arr, ind, acc):
+    if ind == len(arr):
+        return acc
+    return R_S(arr, ind +1, acc + arr[ind])
 
-masiv_hash = dict()
+def max_val(arr, ind, cur_max):
+    if ind == len(arr):
+        return cur_max
+    if arr[ind] > cur_max:
+        cur_max = arr[ind]
+    return max_val(arr, ind + 1, cur_max)
 
-for s in date:
-    if s == '18.10.2013':
-        for i in predmet:
-            if i == 'Русский':
-                for g in second_name:
-                     for h in dati:
-                        print(s, i,g)
-                        masiv_hash[hash(s + i + g)] = h
+def min_val(arr, ind, cur_min):
+    if ind == len(arr):
+        return cur_min
+    if arr[ind] < cur_min:
+        cur_min = arr[ind]
+    return min_val(arr, ind + 1, cur_min)
 
-print(hash('18.10.2013' + 'Русский'+'Корнилин'))
-print(masiv_hash[hash('18.10.2013'+'Русский' +'Корнилин' )])
-#Фильтр через хеширование
-#фамилия > предмет(повторяетс) > дата(повторяетс) > пустое значение(оценка) не повторяется
-#фамилия > предмет > дата > пустое значение
-#фамилия > предмет > дата > пустое значение
-#фамилия > предмет > дата > пустое значение
+arr1 = [1,4,2,3,56]
+mins = min_val(arr1, 0, arr1[0])
+maxs = max_val(arr1, 0, arr1[0])
+result = R_S(arr1, 0, 0)
+
+print('sum:', result, 'min:', mins, 'max:', maxs)
+
+# for n in range(100):
+#     for m in range(100):
+#         if n**2 + m == 99: #n^2 + m = 99
+#             print(n, m)
